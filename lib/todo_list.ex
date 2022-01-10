e9cda1e3f9c58165005b2eabfbc98d3284555a83
gerald-annan <geraldannan400@gmail.com>
2022-01-10 12:35:16 +0000

    provides data structure abstraction for to-do list

---
+++ lib/todo_list.ex
@@ -0,0 +1,21 @@
+defmodule TodoList do
+  def new() do
+    []
+  end
+
+  def add(todoList, new_entry) do
+    todoList ++ [new_entry]
+  end
+
+  def entries(todoList, key) do
+    # func = fn result -> send(pid, {:entries, result}) end
+
+    Enum.filter(todoList, fn item ->
+      item[:date] == key or item[:title] == key
+    end)
+
+    # |> func.()
+
+    # todoList
+  end
+end
